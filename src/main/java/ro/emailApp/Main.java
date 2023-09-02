9b39d5108b14adda0bf2714cfc65b78a2202715e
Vlad <vlad.c.nichifor@gmail.com>
2023-09-02 06:46:34 +0300

    main method to test the email class

--- src/main/java/ro/emailApp/Main.java
+++ src/main/java/ro/emailApp/Main.java
@@ -2,6 +2,11 @@ package ro.emailApp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Email firstUser = new Email("Vlad", "Nichifor");
+        firstUser.setDepartment();
+        firstUser.setMailboxCapacity(500);
+        firstUser.setPassword("abcdefg123456");
+        System.out.println("PASSWORD IS: "+firstUser.getPassword());
+        System.out.println(firstUser);
     }
 }
