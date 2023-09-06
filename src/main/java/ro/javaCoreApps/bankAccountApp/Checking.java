1938a08dc1528dce554d9d6345ca31902521fd55
Vlad <vlad.c.nichifor@gmail.com>
2023-09-06 15:40:17 +0300

    update checking account with requirements

--- src/main/java/ro/javaCoreApps/bankAccountApp/Checking.java
+++ src/main/java/ro/javaCoreApps/bankAccountApp/Checking.java
@@ -7,5 +7,22 @@ public class Checking extends Account {
 
     Checking(String name, String socialSecurityNumber, double initialDeposit) {
         super(name, socialSecurityNumber, initialDeposit);
+        this.accountNumber = '2' + this.accountNumber;
+        setDebitCard();
+        this.showInfo();
+    }
+
+    private void setDebitCard() {
+        debitCardNumber = (int)(Math.random() * Math.pow(10, 12));
+        debitCardPin = (int)(Math.random() * Math.pow(10, 4));
+    }
+    @Override
+    public void showInfo() {
+        super.showInfo();
+        System.out.println(
+                "\tCHECKING ACCOUNT FEATURES: " +
+                "\n\t- Debit card number: " + debitCardNumber+
+                "\n\t- Debit card PIN: " + debitCardPin + '\n'
+        );
     }
 }
