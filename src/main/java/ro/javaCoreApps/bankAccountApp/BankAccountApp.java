d17bb4b83a64d44d814929739791bcc2fafdfe85
Vlad <vlad.c.nichifor@gmail.com>
2023-09-07 16:16:04 +0300

    create accounts from CSV and store them in a list

--- src/main/java/ro/javaCoreApps/bankAccountApp/BankAccountApp.java
+++ src/main/java/ro/javaCoreApps/bankAccountApp/BankAccountApp.java
@@ -1,7 +1,34 @@
 package ro.javaCoreApps.bankAccountApp;
+import ro.javaCoreApps.utilities.CSV;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class BankAccountApp {
     public static void main(String[] args) {
+        List<Account> accounts = new LinkedList<>();
 
+        // read the accounts from CSV file
+        String accountsFile = "/Users/vladdstian/Desktop/javaCoreApps/src/main/java/ro/javaCoreApps/bankAccountApp/NewBankAccounts.csv";
+        List<String[]> clientsList = CSV.read(accountsFile);
+        for(String[] accountHolder: clientsList) {
+            System.out.println("NEW ACCOUNT: ");
+            String name = accountHolder[0];
+            String socialSecurityNumber = accountHolder[1];
+            String accountType = accountHolder[2];
+            double initialAmount = Double.parseDouble(accountHolder[3]);
+            if(accountType.equalsIgnoreCase("savings")) {
+                accounts.add(new Saving(name, socialSecurityNumber, initialAmount));
+            } else if (accountType.equalsIgnoreCase("checking")) {
+                accounts.add(new Checking(name, socialSecurityNumber, initialAmount));
+            } else {
+                System.err.println("ERROR READING ACCOUNT TYPE!");
+            }
+        }
+
+        for(Account acc: accounts) {
+            acc.showInfo();
+            System.out.println("----------------");
+        }
     }
 }
